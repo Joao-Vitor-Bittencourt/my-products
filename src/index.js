506394f832ab682f51767b43739a7a21ea4a95ad
João Bittencourt <joaobittencourtd3v@gmail.com>
2023-01-26 16:47:25 -0300

    :sparkles: initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('creating project')
-})
+  response.send('creating project');
+});
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
